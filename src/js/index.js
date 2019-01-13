4db5bb09a3dc61cabc37c3bbaab2b2545ceb7554
Andrew <andrew-thompson@hotmail.co.uk>
2019-01-13 11:11:06 +0000

    refactor javascript

--- src/js/index.js
+++ src/js/index.js
@@ -3,31 +3,43 @@ import '../css/app.pcss';
 document.addEventListener("DOMContentLoaded", function(){
   const button = document.querySelector('.form__button')
   const container = document.querySelector('.form__container')
+  const input = document.querySelector('.form__input-url')
   const form = container.querySelector('form')
 
   if (button != 'undefined') {
-    button.addEventListener('click', e => {
-      e.preventDefault()
-      let formData = new FormData(form)
-      
-      fetch("/create", {
-          body: formData,
-          method: "POST"
-      })
-      .then(data => {
-        return data.json()
-      })
-      .then(data => {
-        const successMessage = document.querySelector('.form__success')
-        const title = document.querySelector('.form__title').querySelector('h1')
+    button.addEventListener('click', send)
+  }
 
-        successMessage.innerHTML = `Your shortened url for <a href=${data.longUrl}>${data.longUrl}</a> is <a href=${data.shortUrl}>${data.shortUrl}</a>`
-        successMessage.style.display = "flex"
-        
-        title.innerHTML = "Success"
-        
-        container.style.display = "none"
-      })
+  /**
+   * Send formdata to /create endpoint
+   * @param {event} e 
+   */
+  function send(e) {
+    e.preventDefault()
+    let formData = new FormData(form)
+    
+    fetch("/create", {
+        body: formData,
+        method: "POST"
     })
+    .then(data => {
+      return data.json()
+    })
+    .then(data => {
+      input.value = data.shortUrl
+      button.innerHTML = "Copy"
+      button.removeEventListener('click', send)
+      button.addEventListener('click', copy)
+    })
+  }
+
+  /**
+   * Copy input text to clipboard
+   * @param {event} e 
+   */
+  function copy(e) {
+    e.preventDefault()
+    input.select()
+    document.execCommand("copy");
   }
 })
